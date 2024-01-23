937143281be177dce011020dc025a6a498a7670a
RonaldHsiao <ronald032001@yahoo.com.tw>
2024-01-23 21:42:20 +0800

    將結果跟評語輸出到網頁

--- js/bmi.js
+++ js/bmi.js
@@ -1,9 +1,11 @@
-const useraNameEl = document.querySelector("#username");
+const userNameEl = document.querySelector("#username");
 const heightEl = document.querySelector("#height");
 const weightEl = document.querySelector("#weight");
+const bmiEl = document.querySelector("#bmi");
+const commentEl = document.querySelector("#comment");
 
 
-console.log(useraNameEl, heightEl, weightEl);
+console.log(userNameEl, heightEl, weightEl);
 
 
 function calcBmi() {
@@ -16,7 +18,23 @@ function calcBmi() {
     }
 
     let bmi = getBmi(height, weight);
-    console.log(bmi);
+    bmiEl.innerText = "BMI:" + bmi;
+    if (bmi < 18.5) {
+        commentEl.innerText = "體重過輕!";
+    } else if (bmi < 24) {
+        commentEl.innerText = "正常!"
+    } else if (bmi < 27) {
+        commentEl.innerText = "過重!"
+    }
+    else if (bmi < 30) {
+        commentEl.innerText = "輕度肥胖!"
+    }
+    else if (bmi < 35) {
+        commentEl.innerText = "中度肥胖!"
+    } else {
+        commentEl.innerText = "重度肥胖!"
+    }
+    console.log(bmi, comment);
 }
 
 
@@ -28,3 +46,12 @@ function getBmi(height, weight) {
     }
     return bmi.toFixed(2);
 }
+
+function deleteBmi() {
+    let username = userNameEl.value = "";
+    let weight = weightEl.value = "";
+    let height = heightEl.value = "";
+    bmiEl.innerText = "";
+    commentEl.innerText = "";
+}
+
